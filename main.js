2297ed4d1cbb0c70170401dc76315b9228f1bddf
Ignacio Arenas <ignacioarenas2010@hotmail.com>
2022-08-30 19:02:49 -0300

    Primer desafío

---
+++ main.js
@@ -0,0 +1,28 @@
+let llave = prompt("¿Cerraste tu puerta con llave?");
+
+switch(llave) {
+    case "Sí":
+        alert("Todo en orden, seguí con lo tuyo entonces.");
+        console.log("Todo en orden, seguí con lo tuyo entonces.");
+        break;
+    case "Si":
+        alert("Todo en orden, seguí con lo tuyo entonces.");
+        console.log("Todo en orden, seguí con lo tuyo entonces.");
+        break;
+    case "sí":
+        alert("Todo en orden, seguí con lo tuyo entonces.");
+        console.log("Todo en orden, seguí con lo tuyo entonces.");
+        break;
+    case "si":
+        alert("Todo en orden, seguí con lo tuyo entonces.");
+        console.log("Todo en orden, seguí con lo tuyo entonces.");
+        break;
+    case "No":
+        alert("Cerrala, pueden robarte.");
+        console.log("Cerrala, pueden robarte.");
+        break;
+    case "no":
+        alert("Cerrala, pueden robarte.");
+        console.log("Cerrala, pueden robarte.");
+        break;
+}
